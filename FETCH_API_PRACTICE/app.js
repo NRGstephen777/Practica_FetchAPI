5cda9b6f9db4f41746c3d08c6863cd5b54c4ab5a
unknown <st4924@utr.edu.mx>
2026-03-30 19:23:46 -0600

    Third Exercise of the fetch activity

--- FETCH_API_PRACTICE/app.js
+++ FETCH_API_PRACTICE/app.js
@@ -1,60 +1,44 @@
 function mostrarDatos(){
     fetch(" http://localhost:3000/contactos")
     .then((response) => response.json())
+
     .then((data)=>{
         data.data.forEach(element => {
             let tr = document.createElement("tr")
-            console.log(element["nombre"])
             
-            tr.innerHTML=`<td>${element["id"]}</td> <td>${element["nombre"]}</td> <td>${element["telefono"]}</td> <td>Ninguno</trd>`
+            tr.innerHTML=`<td>${element["id"]}</td> <td>${element["nombre"]}</td> 
+            <td>${element["telefono"]}</td> <td> Ninguno </td> 
+            <button class = "btn btn-sm btn-danger" id="delete-btn" >Eliminar</button>`
             
             let tbody = document.getElementById("tablaContactos");
             console.log(tbody)
-            
             tbody.appendChild(tr) 
         });
+        
+        let btn = document.getElementById("delete-btn");
+        console.log(btn)
+        btn.addEventListener("click",()=>{
+        eliminarContacto(contacto.id)
     })
+
+    })
+
 }
 
-fetch("http://localhost:3000/contactos", {
-    method: "POST",                              // Método HTTP
-    headers: {
-        "Content-Type": "application/json",      // Indicar que enviamos JSON
-    },
-    body: JSON.stringify({ nombre: "...", telefono: "..." })  // Los datos
-});
 
-
-
-const form = document.getElementById("contactForm");
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-
-    let nombre = document.getElementById("nombre").value;
-    console.log(nombre)
-    
-    let telefono = document.getElementById("telefono").value;
-    console.log(telefono)
-
-    fetch("http://localhost:3000/contactos", {
-        method: "POST",                              // Método HTTP
+function eliminarContacto(id){
+    fetch(`http://localhost:3000/contactos/${id}`, {
+    method: "DELETE",
         headers: {
-            "Content-Type": "application/json",      // Indicar que enviamos JSON
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify( 
-            { 
-            nombre: nombre, 
-            telefono: telefono,
-            }
-        )  // Los datos
     })
 
-    .then(response => response.json())
-    .then((data) =>
-        mostrarDatos()
-    )
+    .then((data)=>{
+        alert("The contact was deleted")
+    })
 
-});
+    mostrarDatos();
+}
 
-
+mostrarDatos();
